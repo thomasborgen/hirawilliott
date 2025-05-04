b14af0734ecbc6964aff1d1eb6d04caba82bec4c
thomasborgen <thomasborgen91@gmail.com>
2025-05-04 13:45:54 +0200

    use kanjis and katakana to maek UI voice use correct pronounciation

--- hirawilliott/hiragana/database.py
+++ hirawilliott/hiragana/database.py
@@ -3,116 +3,293 @@ from pydantic import BaseModel
 
 class Hiragana(BaseModel):
     character: str
-    romaji: str
     word: str
+    romaji: str
+    hiragana: str
     image: str
 
 
 db = {
     "あ": [
         Hiragana(
-            character="あ", romaji="arupaka", word="あるぱか", image="arupaka.png"
+            character="あ",
+            word="アルパカ",
+            romaji="arupaka",
+            hiragana="あるぱか",
+            image="arupaka.png",
         ),
         Hiragana(
-            character="あ", romaji="araiguma", word="あらいぐま", image="araiguma.png"
+            character="あ",
+            word="浣熊",
+            romaji="araiguma",
+            hiragana="あらいぐま",
+            image="araiguma.png",
         ),
     ],
-    "い": [Hiragana(character="い", romaji="inu", word="犬", image="inu.png")],
-    "う": [
-        Hiragana(character="う", romaji="uma", word="馬", image="uma.png"),
-        Hiragana(character="う", romaji="usagi", word="兎", image="usagi.png"),
-        Hiragana(character="う", romaji="ushi", word="牛", image="ushi.png"),
+    "い": [
+        Hiragana(
+            character="い", word="犬", romaji="inu", hiragana="いぬ", image="inu.png"
+        )
     ],
-    "え": [Hiragana(character="え", romaji="ebi", word="蝦", image="ebi.png")],
-    "お": [Hiragana(character="お", romaji="ookami", word="狼", image="ookami.png")],
+    "う": [
+        Hiragana(
+            character="う", word="馬", romaji="uma", hiragana="うま", image="uma.png"
+        ),
+        Hiragana(
+            character="う",
+            word="兎",
+            romaji="usagi",
+            hiragana="うさぎ",
+            image="usagi.png",
+        ),
+        Hiragana(
+            character="う", word="牛", romaji="ushi", hiragana="うし", image="ushi.png"
+        ),
+    ],
+    "え": [
+        Hiragana(
+            character="え", word="蝦", romaji="ebi", hiragana="えび", image="ebi.png"
+        )
+    ],
+    "お": [
+        Hiragana(
+            character="お",
+            word="狼",
+            romaji="ookami",
+            hiragana="おおかみ",
+            image="ookami.png",
+        )
+    ],
     # か行
-    "か": [Hiragana(character="か", romaji="kaba", word="カバ", image="kaba.png")],
-    "き": [Hiragana(character="き", romaji="kirin", word="麒麟", image="kirin.png")],
+    "か": [
+        Hiragana(
+            character="か",
+            word="カバ",
+            romaji="kaba",
+            hiragana="かば",
+            image="kaba.png",
+        )
+    ],
+    "き": [
+        Hiragana(
+            character="き",
+            word="キリン",
+            romaji="kirin",
+            hiragana="きりん",
+            image="kirin.png",
+        )
+    ],
     "く": [
-        Hiragana(character="く", romaji="kuma", word="熊", image="kuma.png"),
-        Hiragana(character="く", romaji="kurohyou", word="黒豹", image="kurohyou.png"),
+        Hiragana(
+            character="く", word="熊", romaji="kuma", hiragana="くま", image="kuma.png"
+        ),
+        Hiragana(
+            character="く",
+            word="黒豹",
+            romaji="kurohyou",
+            hiragana="くろひょう",
+            image="kurohyou.png",
+        ),
     ],
     "け": [
-        Hiragana(character="け", romaji="kemushi", word="毛虫", image="kemushi.png")
+        Hiragana(
+            character="け",
+            word="毛虫",
+            romaji="kemushi",
+            hiragana="けむし",
+            image="kemushi.png",
+        )
     ],
     "こ": [
         Hiragana(
-            character="こ", romaji="koumori", word="こうもり", image="koumori.png"
+            character="こ",
+            word="蝙蝠",
+            romaji="koumori",
+            hiragana="こうもり",
+            image="koumori.png",
         ),
     ],
     # さ行
-    "さ": [Hiragana(character="さ", romaji="sai", word="さい", image="sai.png")],
-    "し": [
+    "さ": [
         Hiragana(
-            character="し", romaji="shimauma", word="しまうま", image="shimauma.png"
+            character="さ", word="サイ", romaji="sai", hiragana="さい", image="sai.png"
         )
     ],
-    "す": [Hiragana(character="す", romaji="suika", word="すいか", image="suika.png")],
+    "し": [
+        Hiragana(
+            character="し",
+            word="シマウマ",
+            romaji="shimauma",
+            hiragana="しまうま",
+            image="shimauma.png",
+        )
+    ],
+    "す": [
+        Hiragana(
+            character="す",
+            word="西瓜",
+            romaji="suika",
+            hiragana="すいか",
+            image="suika.png",
+        )
+    ],
     "せ": [
-        Hiragana(character="せ", romaji="seiuchi", word="せいうち", image="seiuchi.png")
+        Hiragana(
+            character="せ",
+            word="セイウチ",
+            romaji="seiuchi",
+            hiragana="せいうち",
+            image="seiuchi.png",
+        )
     ],
     "そ": [
-        Hiragana(character="そ", romaji="soujiki", word="そうじき", image="soujiki.png")
+        Hiragana(
+            character="そ",
+            word="掃除機",
+            romaji="soujiki",
+            hiragana="そうじき",
+            image="soujiki.png",
+        )
     ],
-    "ぞ": [Hiragana(character="ぞ", romaji="zou", word="ぞう", image="zou.png")],
+    "ぞ": [
+        Hiragana(
+            character="ぞ", word="蔵", romaji="zou", hiragana="ぞう", image="zou.png"
+        )
+    ],
     # た行
     "た": [
-        Hiragana(character="た", romaji="tanuki", word="たぬき", image="tanuki.png")
+        Hiragana(
+            character="た",
+            word="タヌキ",
+            romaji="tanuki",
+            hiragana="たぬき",
+            image="tanuki.png",
+        )
     ],
-    "ち": [Hiragana(character="ち", romaji="chou", word="蝶", image="chou.png")],
+    "ち": [
+        Hiragana(
+            character="ち", word="", romaji="chou", hiragana="蝶", image="chou.png"
+        )
+    ],
     "つ": [
-        Hiragana(character="つ", romaji="tsubame", word="つばめ", image="tsubame.png")
+        Hiragana(
+            character="つ",
+            word="",
+            romaji="tsubame",
+            hiragana="つばめ",
+            image="tsubame.png",
+        )
     ],
     "て": [
         Hiragana(
             character="て",
+            word="",
             romaji="tentoumushi",
-            word="てんとうむし",
+            hiragana="てんとうむし",
             image="tentoumushi.png",
         )
     ],
     "と": [
-        Hiragana(character="と", romaji="tonakai", word="となかい", image="tonakai.png")
+        Hiragana(
+            character="と",
+            word="",
+            romaji="tonakai",
+            hiragana="となかい",
+            image="tonakai.png",
+        )
     ],
     # な行
     "な": [
         Hiragana(
-            character="な", romaji="namekuji", word="なめくじ", image="namekuji.png"
+            character="な",
+            word="蛞蝓",
+            romaji="namekuji",
+            hiragana="なめくじ",
+            image="namekuji.png",
         )
     ],
     "に": [
         Hiragana(
-            character="に", romaji="niwatori", word="にわとり", image="niwatori.png"
+            character="に",
+            word="ニワトリ",
+            romaji="niwatori",
+            hiragana="にわとり",
+            image="niwatori.png",
         )
     ],
-    "ぬ": [Hiragana(character="ぬ", romaji="nuu", word="ぬー", image="nuu.png")],
-    "ね": [Hiragana(character="ね", romaji="neko", word="ねこ", image="neko.png")],
-    "の": [Hiragana(character="の", romaji="nomi", word="のみ", image="nomi.png")],
+    "ぬ": [
+        Hiragana(
+            character="ぬ", word="ヌー", romaji="nuu", hiragana="ぬー", image="nuu.png"
+        )
+    ],
+    "ね": [
+        Hiragana(
+            character="ね", word="猫", romaji="neko", hiragana="ねこ", image="neko.png"
+        )
+    ],
+    "の": [
+        Hiragana(
+            character="の", word="蚤", romaji="nomi", hiragana="のみ", image="nomi.png"
+        )
+    ],
     # は行
     "も": [
-        Hiragana(character="も", romaji="mogura", word="もぐら", image="mogura.png")
+        Hiragana(
+            character="も",
+            word="土竜",
+            romaji="mogura",
+            hiragana="もぐら",
+            image="mogura.png",
+        )
     ],
     "ご": [
-        Hiragana(character="こ", romaji="gorira", word="ごりら", image="gorira.png"),
+        Hiragana(
+            character="こ",
+            word="",
+            romaji="gorira",
+            hiragana="ごりら",
+            image="gorira.png",
+        ),
     ],
-    "り": [Hiragana(character="り", romaji="risu", word="りす", image="risu.png")],
+    "り": [
+        Hiragana(
+            character="り", word="", romaji="risu", hiragana="りす", image="risu.png"
+        )
+    ],
     "れ": [
         Hiragana(
             character="れ",
+            word="",
             romaji="ressaapanda",
-            word="れっさーぱんだ",
+            hiragana="れっさーぱんだ",
             image="ressaapanda.png",
         )
     ],
     "は": [
         Hiragana(
-            character="は", romaji="hamsutaa", word="はむすたー", image="hamusutaa.png"
+            character="は",
+            word="",
+            romaji="hamsutaa",
+            hiragana="はむすたー",
+            image="hamusutaa.png",
         )
     ],
     "ひ": [
-        Hiragana(character="ひ", romaji="hitsuji", word="ひつじ", image="hitsuji.png")
+        Hiragana(
+            character="ひ",
+            word="",
+            romaji="hitsuji",
+            hiragana="ひつじ",
+            image="hitsuji.png",
+        )
     ],
     "ら": [
-        Hiragana(character="ら", romaji="rakuda", word="らくだ", image="rakuda.png")
+        Hiragana(
+            character="ら",
+            word="",
+            romaji="rakuda",
+            hiragana="らくだ",
+            image="rakuda.png",
+        )
     ],
 }
